e75121046904b9308016a284eb4bdb1b3577fe65
JLoo <junzhongloo@gmail.com>
2015-08-07 09:01:05 +0800

    set up initial layout file --title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,17 +4,21 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    # select title HTML returns "home... app" ##case sensitive!
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
   #test NAME .. get about controller .. return success status
   test "should get about" do
   	get :about
   	assert_response :success
+  	assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end	
 
 end
